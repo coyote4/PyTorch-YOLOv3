19dd194fb422409f9b7c3355cbb0f93b690440d4
coyote4 <63921448+coyote4@users.noreply.github.com>
2020-04-19 04:19:09 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,24 +1,18 @@
-from __future__ import division
-
 from models import *
 from utils.utils import *
 from utils.datasets import *
 from utils.parse_config import *
-
 import os
 import sys
 import time
 import datetime
 import argparse
-import tqdm
-
 import torch
 from torch.utils.data import DataLoader
 from torchvision import datasets
 from torchvision import transforms
 from torch.autograd import Variable
 import torch.optim as optim
-
 
 def evaluate(model, path, iou_thres, conf_thres, nms_thres, img_size, batch_size):
     model.eval()
@@ -52,22 +46,20 @@ def evaluate(model, path, iou_thres, conf_thres, nms_thres, img_size, batch_size
     # Concatenate sample statistics
     true_positives, pred_scores, pred_labels = [np.concatenate(x, 0) for x in list(zip(*sample_metrics))]
     precision, recall, AP, f1, ap_class = ap_per_class(true_positives, pred_scores, pred_labels, labels)
-
     return precision, recall, AP, f1, ap_class
-
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--batch_size", type=int, default=8, help="size of each image batch")
     parser.add_argument("--model_def", type=str, default="config/yolov3.cfg", help="path to model definition file")
     parser.add_argument("--data_config", type=str, default="config/coco.data", help="path to data config file")
-    parser.add_argument("--weights_path", type=str, default="weights/yolov3.weights", help="path to weights file")
     parser.add_argument("--class_path", type=str, default="data/coco.names", help="path to class label file")
     parser.add_argument("--iou_thres", type=float, default=0.5, help="iou threshold required to qualify as detected")
     parser.add_argument("--conf_thres", type=float, default=0.001, help="object confidence threshold")
     parser.add_argument("--nms_thres", type=float, default=0.5, help="iou thresshold for non-maximum suppression")
     parser.add_argument("--n_cpu", type=int, default=8, help="number of cpu threads to use during batch generation")
     parser.add_argument("--img_size", type=int, default=416, help="size of each image dimension")
+    parser.add_argument("--model_path", type=str, default='', help="path to model")
     opt = parser.parse_args()
     print(opt)
 
@@ -79,12 +71,8 @@ if __name__ == "__main__":
 
     # Initiate model
     model = Darknet(opt.model_def).to(device)
-    if opt.weights_path.endswith(".weights"):
-        # Load darknet weights
-        model.load_darknet_weights(opt.weights_path)
-    else:
-        # Load checkpoint weights
-        model.load_state_dict(torch.load(opt.weights_path))
+    # Load checkpoint weights
+    model.load_state_dict(torch.load(opt.model_path))
 
     print("Compute mAP...")
 
